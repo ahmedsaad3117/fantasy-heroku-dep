2a5b42def33475b1c1fbb7db152e5974d8b5a9fd
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2022-07-14 06:32:59 +0200

    delete admin

--- src/componant/MainRoutes.js
+++ src/componant/MainRoutes.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import AdminPanel from "../pages/admin/adminPanel";
 import Home from "../pages/Home/Home";
 
 const MainRoutes = () => {
@@ -7,7 +6,6 @@ const MainRoutes = () => {
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/admin" element={<AdminPanel />} />
       </Routes>
     </>
   );
