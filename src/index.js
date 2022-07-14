8b6da71e428bfc4af65b58dfc98cd7a904ab4e75
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2022-07-14 07:41:26 +0200

    fix bug

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import store from "./store/store";
-import reportWebVitals from "./reportWebVitals";
+
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -26,4 +26,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
